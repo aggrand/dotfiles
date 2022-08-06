a63f88a77957b5edb250a8f88064b71ae38f1654
Carl Raiden Worley <carl.worley@10gen.com>
2022-08-06 14:34:31 -0400

    track more things

--- install.sh
+++ install.sh
@@ -7,15 +7,16 @@ sudo xcode-select --install
 
 set -eo pipefail
 
-# Install/Update Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
+    echo "Installing homebrew."
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
+    echo "Updating homebrew."
     brew update
 fi
 
-# Bootstrap python
+echo "Bootstrapping python."
 brew install pyenv
 brew upgrade pyenv
 if brew ls --versions pyenv >/dev/null; then
@@ -23,10 +24,12 @@ if brew ls --versions pyenv >/dev/null; then
 else
     HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv
 fi
-source ./pyenv.sh
+source .bashrc
 pyenv install -s 3.9.9
 pyenv global 3.9.9
 sudo pip3 install --upgrade pip
 
-# Run the ansible playbook.
+echo "Installing ansible."
 pip3 install ansible
+
+echo "Running ansible."
