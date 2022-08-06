3220b2d39409f4bc7a8c908db6975184c6411a7c
Carl Raiden Worley <carl.worley@10gen.com>
2022-08-06 13:29:54 -0400

    pyenv working correctly

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
-set -ux
+set -u
 
-# May fail if already installed.
+echo "Note: Installing xcode-select may fail if already present. Errors can be ignored."
 sudo xcode-select --install
 
 set -eo pipefail
 
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install/Update Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
 
 # Bootstrap python
-brew update
 brew install pyenv
+brew upgrade pyenv
+if brew ls --versions pyenv >/dev/null; then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade pyenv 
+else
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv
+fi
 source ./pyenv.sh
 pyenv install -s 3.9.9
 pyenv global 3.9.9
+sudo pip3 install --upgrade pip
+
+# Run the ansible playbook.
+pip3 install ansible
--- pyenv.sh
+++ pyenv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-export PYENV_ROOT = $HOME/.pyenv
+export PROMPT_COMMAND=""
+export PYENV_ROOT=$HOME/.pyenv
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
