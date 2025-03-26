70548f575aee4befe3177e7de96ff82efdf0dd91
Raiden Worley <raiden.worley@alloy.com>
2025-03-26 18:06:58 -0400

    No roam ui start

--- doom.d/config.el
+++ doom.d/config.el
@@ -579,4 +579,5 @@
     (setq org-roam-ui-sync-theme t
           org-roam-ui-follow t
           org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
+          ;;org-roam-ui-open-on-start t
+))
