fce0fea243334dec35da1304a15b6cccfa3c8562
Raiden Worley <raiden.worley@alloy.com>
2025-03-29 11:36:00 -0400

    feat: Add nvim config

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,20 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 4
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.clipboard = "unnamedplus"
